83e15a2af8a435474d627d7c598a41ff9f137ff1
Vegetabbel <veikkahuttunen@kamk.fi>
2016-04-08 22:00:43 +0300

    Fade hienosti kopioitu vampyyripelistä

---
+++ Assets/Scripts/ApacheEffect.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ApacheEffect : MonoBehaviour
+{
+
+    Player player1Script;
+
+    public SpriteRenderer background;
+    float lerpTime = 0.2f;
+    float currentLerpTime;
+    bool resetLerp;
+    Color fadeStartValue;
+    Color fadeEndValue;
+
+    void Start()
+    {
+        player1Script = GameObject.Find("Player1").GetComponent<Player>();
+    }
+
+    void Update()
+    {
+        FadeRooms();
+    }
+    void FadeRooms()
+    {
+        if (player1Script.GetActiveStatus() == true)
+        {
+            if (resetLerp)
+            {
+                currentLerpTime = 0;
+                resetLerp = false;
+                fadeStartValue = background.color;
+            }
+
+            fadeEndValue = new Color(background.color.r, background.color.g, background.color.b, 0.0f);
+
+            currentLerpTime += Time.deltaTime;
+
+            if (currentLerpTime > lerpTime)
+            {
+                currentLerpTime = lerpTime;
+            }
+
+            float perc = currentLerpTime / lerpTime;
+
+            Color newFadeValue = Color.Lerp(fadeStartValue, fadeEndValue, perc);
+
+            background.color = newFadeValue;
+        }
+        else
+        {
+            if (!resetLerp)
+            {
+                currentLerpTime = 0;
+                resetLerp = true;
+                fadeStartValue = background.color;
+            }
+
+            fadeEndValue = new Color(background.color.r, background.color.g, background.color.b, 1.0f);
+
+            currentLerpTime += Time.deltaTime;
+
+            if (currentLerpTime > lerpTime)
+            {
+                currentLerpTime = lerpTime;
+            }
+
+            float perc = currentLerpTime / lerpTime;
+
+            Color newFadeValue = Color.Lerp(fadeStartValue, fadeEndValue, perc);
+
+            background.color = newFadeValue;
+        }
+    }
+}
