573f9703ac2e2754a94138305038e3daac8f5f24
Luzer59 <luzer59@hotmail.com>
2016-04-09 20:15:55 +0300

    Hhhngggghh!!!

--- Assets/PopupEffect.cs
+++ Assets/PopupEffect.cs
@@ -1,7 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+public enum UiItemType { Text, Image }
+
 public class PopupEffect : MonoBehaviour
 {
+    public UiItemType type;
 
+    public void Activate(float duration)
+    {
+        StartCoroutine(Effect(duration));
+    }
+
+    IEnumerator Effect(float duration)
+    {
+        yield return new WaitForSeconds(2f);
+        if (type == UiItemType.Text)
+        {
+
+        }
+    }
 }
