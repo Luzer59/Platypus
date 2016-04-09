9ea98d707512b032eadc5dd7e762fdbd9348c71f
Vegetabbel <veikkahuttunen@kamk.fi>
2016-04-09 11:46:49 +0300

    Apache juttuja kesken vielä :^)

--- Assets/Scripts/ApacheEffect.cs
+++ Assets/Scripts/ApacheEffect.cs
@@ -1,12 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class ApacheEffect : MonoBehaviour
 {
+    Player1 player1Script;
+    GameController gc;
 
-    PlayerBase player1Script;
+    public GameObject apache;
+    List<GameObject> apacheList = new List<GameObject>();
+    public int apacheCount;
 
-    public SpriteRenderer background;
+    public List<Sprite> levelTypes = new List<Sprite>();
+    public List<Sprite> apacheTypes = new List<Sprite>();
+
+    int currentLevelType = 0;
+    SpriteRenderer background;
+    SpriteRenderer backBackground;
     float lerpTime = 0.2f;
     float currentLerpTime;
     bool resetLerp;
@@ -17,7 +27,14 @@ public class ApacheEffect : MonoBehaviour
 
     void Start()
     {
-        player1Script = GameObject.Find("Player1").GetComponent<PlayerBase>();
+        player1Script = GameObject.Find("Player1").GetComponent<Player1>();
+        gc = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+        background = GameObject.FindGameObjectWithTag("Background").GetComponent<SpriteRenderer>();
+        backBackground = GameObject.FindGameObjectWithTag("BackBackground").GetComponent<SpriteRenderer>();
+
+        SetLevelType();
+        SpawnApaches();
+        SetApacheSprites();           
     }
 
     void Update()
@@ -75,9 +92,47 @@ public class ApacheEffect : MonoBehaviour
             background.color = newFadeValue;
         }
     }
+    void SetLevelType()
+    {
+        //set level type (= background)
+        Sprite newLevelBack = null;
+        Sprite oldLevelBack = newLevelBack;
+        while (newLevelBack == null || newLevelBack == oldLevelBack)
+        {
+            currentLevelType = Random.Range(0, levelTypes.Count);
+            newLevelBack = levelTypes[currentLevelType];
+            background.sprite = newLevelBack;
+            backBackground.sprite = newLevelBack;
+        }
+    }
     void SpawnApaches()
     {
-        Camera.main.ViewportToWorldPoint(new Vector3(0, apacheMinHeight, 0));
-        Camera.main.ViewportToWorldPoint(new Vector3(0, 1, 0));
+        for (int i = 0; i < apacheCount; i++)
+        {
+            GameObject instance = (GameObject)Instantiate(apache, new Vector3(0, 0, 0), Quaternion.identity);
+            apacheList.Add(instance);            
+        }
+    }
+    void SetApacheSprites()
+    {
+        //sprites depending on level type
+        for (int i = 0; i < apacheList.Count; i++)
+        {
+            switch (currentLevelType)
+            {
+                case 0:
+                    apacheList[i].GetComponent<SpriteRenderer>().sprite = apacheTypes[Random.Range(0, 2)];
+                    break;
+                case 1:
+                    apacheList[i].GetComponent<SpriteRenderer>().sprite = apacheTypes[Random.Range(1, 3)];
+                    break;
+                case 2:
+                    apacheList[i].GetComponent<SpriteRenderer>().sprite = apacheTypes[Random.Range(2, 4)];
+                    break;
+                default:
+                    apacheList[i].GetComponent<SpriteRenderer>().sprite = apacheTypes[Random.Range(0, 1)];
+                    break;
+            }
+        }
     }
 }
