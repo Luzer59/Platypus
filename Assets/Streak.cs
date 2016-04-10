7608fb14a79dc699e76ce942eb99954521de8e62
Luzer59 <luzer59@hotmail.com>
2016-04-10 14:51:54 +0300

    WINSTREAKS!!! - GODLIKE -

--- Assets/Streak.cs
+++ Assets/Streak.cs
@@ -10,42 +10,53 @@ public class Streak : MonoBehaviour
     public int current1Value = 0;
     public int current2Value = 0;
 
+    private GameController gc;
+
+    void Start()
+    {
+        gc = GameController.instance;
+    }
+
     void Update()
     {
-        if (GameController.instance.player1Streak != current1Value)
+        if (gc.player1Streak != current1Value)
         {
-            current1Value = GameController.instance.player1Streak;
-            if (current1Value == 0)
+            current1Value = gc.player1Streak;
+            if (player2Indicator)
             {
-                player1Indicator.text = "0 Streak";
-                player1Indicator.enabled = false;
-            }
-            else
-            {
-                player1Indicator.text = current1Value + " Streak";
-                if (!player1Indicator.enabled)
+                if (current1Value == 0)
                 {
-                    player1Indicator.enabled = true;
+                    player1Indicator.text = "0 Streak";
+                    player1Indicator.enabled = false;
+                }
+                else
+                {
+                    player1Indicator.text = current1Value + " Streak";
+                    if (!player1Indicator.enabled)
+                    {
+                        player1Indicator.enabled = true;
+                    }
                 }
             }
         }
-        print(GameController.instance.player2Streak + " : " + current2Value);
-        if (GameController.instance.player2Streak != current2Value)
+        if (gc.player2Streak != current2Value)
         {
-            current2Value = GameController.instance.player2Streak;
-            print(current2Value);
-            if (current2Value == 0)
+            current2Value = gc.player2Streak;
+            if (player2Indicator)
             {
-                player2Indicator.text = "0 Streak";
-                player2Indicator.enabled = false;
-            }
-            else
-            {
-                if (!player2Indicator.enabled)
+                if (current2Value == 0)
                 {
-                    player2Indicator.enabled = true;
+                    player2Indicator.text = "0 Streak";
+                    player2Indicator.enabled = false;
                 }
-                player2Indicator.text = current2Value + " Streak";
+                else
+                {
+                    if (!player2Indicator.enabled)
+                    {
+                        player2Indicator.enabled = true;
+                    }
+                    player2Indicator.text = current2Value + " Streak";
+                }
             }
         }
     }
