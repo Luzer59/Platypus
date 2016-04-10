3ca14612829247feff8b488de9e9e5c40bcc66d6
Luzer59 <luzer59@hotmail.com>
2016-04-10 16:01:51 +0300

    Äänet paikallaaan taas kerrraaaaaabb

--- Assets/Scripts/CustomAnimator.cs
+++ Assets/Scripts/CustomAnimator.cs
@@ -15,6 +15,7 @@ public class CustomAnimator : MonoBehaviour
     public PlayerSprites[] sprites;
     public PlayerBase player;
     public float[] scale;
+    public int[] order;
 
     private SpriteRenderer sr;
     private int currentSpriteIndex = 0;
@@ -29,6 +30,7 @@ public class CustomAnimator : MonoBehaviour
     {
         currentSpriteIndex = GameController.instance.currentSpriteSet;
         transform.localScale = new Vector3(scale[currentSpriteIndex], scale[currentSpriteIndex], scale[currentSpriteIndex]);
+        sr.sortingOrder = order[currentSpriteIndex];
     }
 
     void Update()
